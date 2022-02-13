dff0fec824addfdb1a3c216652b76284f0ec919d
moto <mnxamoto@mail.ru>
2022-02-13 19:44:55 +0300

    Загрузил проект

---
+++ Перевод картинок/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Перевод_картинок
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Распознать текст";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(417, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 375);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(8, 6);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(403, 375);
+            this.textBox2.TabIndex = 2;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(978, 399);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(970, 373);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(970, 373);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(964, 367);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 399);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ Перевод картинок/Form1.cs
@@ -0,0 +1,143 @@
+﻿using Google.Cloud.Translation.V2;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace Перевод_картинок
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string pathImage = openFileDialog1.FileName;
+                string text = "";
+
+                Page page = OCR(pathImage);
+                //text = page.GetText();
+                //textBox2.Text = text;
+                //textBox1.Text = Translate(text);
+
+                Bitmap bmp = new Bitmap(pathImage);
+                Graphics g = Graphics.FromImage(bmp);
+                SolidBrush brushWhite = new SolidBrush(Color.White);
+                SolidBrush brushBlack = new SolidBrush(Color.Black);
+                Font drawFont = new Font("Times New Roman", 16);
+
+                var iterator = page.GetIterator();
+                PageIteratorLevel pageIteratorLevel = PageIteratorLevel.Block;
+
+                iterator.Begin();
+
+                List<string> texts = new List<string>();
+                List<Rectangle> rectangles = new List<Rectangle>();
+
+                do
+                {
+                    text = iterator.GetText(pageIteratorLevel);
+                    //text = Translate(text);
+                    texts.Add(text);
+
+                    iterator.TryGetBoundingBox(pageIteratorLevel, out Rect bounds);
+
+                    Rectangle rectangle = new Rectangle(bounds.X1, bounds.Y1, bounds.Width, bounds.Height);
+                    rectangles.Add(rectangle);
+                }
+                while (iterator.Next(pageIteratorLevel));
+
+                iterator.Dispose();
+
+                texts = Translate(texts);
+
+                for (int i = 0; i < texts.Count; i++)
+                {
+                    g.FillRectangle(brushWhite, rectangles[i]);
+                    g.DrawString(texts[i], drawFont, brushBlack, rectangles[i]);
+                }
+
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private Page OCR(string pathImage)
+        {
+            var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default);
+            Pix img = Pix.LoadFromFile(pathImage);
+            Page recognizedPage = engine.Process(img);
+            return recognizedPage;
+        }
+
+        public List<string> Translate(List<string> texts)
+        {
+            string text = "";
+
+            int n = texts.Count;
+
+            foreach (var item in texts)
+            {
+                text += $"{item}. ";
+            }
+            //text = text.Replace("\n", "%0A");
+            text = text.Replace("\n", " ");
+            text = text.Replace("|", "I");
+
+            //text = "hello I'm moto ";
+
+            string toLanguage = "ru"; //English
+            string fromLanguage = "en"; //Deutsch
+            string https = "https://";
+            //string url = $"{https}translate.google.ru/?hl=ru&sl={fromLanguage}&tl={toLanguage}&text={text}&op=translate";
+            string url = $"{https}translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={HttpUtility.UrlEncode(text)}";
+            //https://translate.google.ru/?hl=ru&sl=en&tl=ru&text=moto&op=translate
+            
+            var webClient = new WebClient
+            {
+                Encoding = System.Text.Encoding.UTF8
+            };
+            var file = webClient.DownloadString(url);
+            try
+            {
+                //result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+                JArray fileJSON = (JArray)JsonConvert.DeserializeObject(file);
+                List<string> result = new List<string>();
+
+                for (int i = 0; i < n; i++)
+                {
+                    result.Add((string)fileJSON[0][i][0]);
+                    //result += line[i][0];
+                }
+
+                return result;
+            }
+            catch
+            {
+                throw;
+                //return "Error";
+            }
+        }
+
+        public String Translate2(string text)
+        {
+            var client = TranslationClient.Create();
+            var response = client.TranslateText(text, LanguageCodes.Russian, LanguageCodes.English);
+            return response.TranslatedText;
+        }
+    }
+}
