ff0adfb04eb9712498ba1b82641e5149c564f4d4
moto <mnxamoto@mail.ru>
2022-02-13 21:48:25 +0300

    Добавил режим выделения области

--- Перевод картинок/Form1.Designer.cs
+++ Перевод картинок/Form1.Designer.cs
@@ -34,21 +34,25 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(8, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.Size = new System.Drawing.Size(141, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Распознать текст";
+            this.button1.Text = "Выбрать изображение";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -85,6 +89,10 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox3);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.pictureBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
@@ -94,6 +102,45 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Распознать область";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Распознать по блокам";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 122);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(143, 90);
+            this.textBox3.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(964, 367);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // tabPage2
             // 
@@ -107,14 +154,15 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // button4
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(964, 367);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.button4.Location = new System.Drawing.Point(6, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Распознать текст";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
@@ -127,9 +175,10 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -144,6 +193,10 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Перевод картинок/Form1.cs
+++ Перевод картинок/Form1.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -23,55 +24,20 @@ namespace Перевод_картинок
             InitializeComponent();
         }
 
+        private Point mousePos1;
+        private Point mousePos2;
+        private Rectangle rectangleArea;
+
+        string pathImage;
+        Bitmap bmp;
+
         private void button1_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                string pathImage = openFileDialog1.FileName;
-                string text = "";
+                pathImage = openFileDialog1.FileName;
 
-                Page page = OCR(pathImage);
-                //text = page.GetText();
-                //textBox2.Text = text;
-                //textBox1.Text = Translate(text);
-
-                Bitmap bmp = new Bitmap(pathImage);
-                Graphics g = Graphics.FromImage(bmp);
-                SolidBrush brushWhite = new SolidBrush(Color.White);
-                SolidBrush brushBlack = new SolidBrush(Color.Black);
-                Font drawFont = new Font("Times New Roman", 16);
-
-                var iterator = page.GetIterator();
-                PageIteratorLevel pageIteratorLevel = PageIteratorLevel.Block;
-
-                iterator.Begin();
-
-                List<string> texts = new List<string>();
-                List<Rectangle> rectangles = new List<Rectangle>();
-
-                do
-                {
-                    text = iterator.GetText(pageIteratorLevel);
-                    //text = Translate(text);
-                    texts.Add(text);
-
-                    iterator.TryGetBoundingBox(pageIteratorLevel, out Rect bounds);
-
-                    Rectangle rectangle = new Rectangle(bounds.X1, bounds.Y1, bounds.Width, bounds.Height);
-                    rectangles.Add(rectangle);
-                }
-                while (iterator.Next(pageIteratorLevel));
-
-                iterator.Dispose();
-
-                texts = Translate(texts);
-
-                for (int i = 0; i < texts.Count; i++)
-                {
-                    g.FillRectangle(brushWhite, rectangles[i]);
-                    g.DrawString(texts[i], drawFont, brushBlack, rectangles[i]);
-                }
-
+                bmp = new Bitmap(pathImage);
                 pictureBox1.Image = bmp;
             }
         }
@@ -80,6 +46,14 @@ namespace Перевод_картинок
         {
             var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default);
             Pix img = Pix.LoadFromFile(pathImage);
+            Page recognizedPage = engine.Process(img);
+            return recognizedPage;
+        }
+
+        private Page OCR(byte[] image)
+        {
+            var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default);
+            Pix img = Pix.LoadFromMemory(image);
             Page recognizedPage = engine.Process(img);
             return recognizedPage;
         }
@@ -120,8 +94,50 @@ namespace Перевод_картинок
 
                 for (int i = 0; i < n; i++)
                 {
-                    result.Add((string)fileJSON[0][i][0]);
+                    string a = fileJSON[0][i][0].ToString();
+                    a = a.Substring(0, a.Length - 1);
+                    result.Add(a);
                     //result += line[i][0];
+                }
+
+                return result;
+            }
+            catch
+            {
+                throw;
+                //return "Error";
+            }
+        }
+
+        public string Translate(string text)
+        {
+            //text = text.Replace("\n", "%0A");
+            text = text.Replace("\n", " ");
+            text = text.Replace("|", "I");
+
+            //text = "hello I'm moto ";
+
+            string toLanguage = "ru"; //English
+            string fromLanguage = "en"; //Deutsch
+            string https = "https://";
+            //string url = $"{https}translate.google.ru/?hl=ru&sl={fromLanguage}&tl={toLanguage}&text={text}&op=translate";
+            string url = $"{https}translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={HttpUtility.UrlEncode(text)}";
+            //https://translate.google.ru/?hl=ru&sl=en&tl=ru&text=moto&op=translate
+
+            var webClient = new WebClient
+            {
+                Encoding = System.Text.Encoding.UTF8
+            };
+            var file = webClient.DownloadString(url);
+            try
+            {
+                //result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+                JArray fileJSON = (JArray)JsonConvert.DeserializeObject(file);
+                string result = "";
+
+                foreach (var fileLine in fileJSON[0])
+                {
+                    result += fileLine[0];
                 }
 
                 return result;
@@ -138,6 +154,191 @@ namespace Перевод_картинок
             var client = TranslationClient.Create();
             var response = client.TranslateText(text, LanguageCodes.Russian, LanguageCodes.English);
             return response.TranslatedText;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousePos1 = e.Location;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousePos2 = e.Location;
+            rectangleArea = GetRectangle(mousePos1, mousePos2);
+
+            textBox3.Text = 
+                $"X = {rectangleArea.X}\r\n" +
+                $"Y = {rectangleArea.Y}\r\n" +
+                $"Ширина = {rectangleArea.Width}\r\n" +
+                $"Высота = {rectangleArea.Height}";
+        }
+
+        private Rectangle GetRectangle(Point p1, Point p2)
+        {
+            var x1 = Math.Min(p1.X, p2.X);
+            var x2 = Math.Max(p1.X, p2.X);
+            var y1 = Math.Min(p1.Y, p2.Y);
+            var y2 = Math.Max(p1.Y, p2.Y);
+            return new Rectangle(x1, y1, x2 - x1, y2 - y1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = "";
+
+            Page page = OCR(pathImage);
+            //text = page.GetText();
+            //textBox2.Text = text;
+            //textBox1.Text = Translate(text);
+
+            Bitmap bmp = new Bitmap(pathImage);
+            Graphics g = Graphics.FromImage(bmp);
+            SolidBrush brushWhite = new SolidBrush(Color.White);
+            SolidBrush brushBlack = new SolidBrush(Color.Black);
+            Pen penBlack = new Pen(Color.Black);
+            Font drawFont = new Font("Times New Roman", 16);
+
+            var iterator = page.GetIterator();
+            PageIteratorLevel pageIteratorLevel = PageIteratorLevel.Block;
+
+            iterator.Begin();
+
+            List<string> texts = new List<string>();
+            List<Rectangle> rectangles = new List<Rectangle>();
+
+            do
+            {
+                text = iterator.GetText(pageIteratorLevel);
+                //text = Translate(text);
+                texts.Add(text);
+
+                iterator.TryGetBoundingBox(pageIteratorLevel, out Rect bounds);
+
+                Rectangle rectangle = new Rectangle(bounds.X1, bounds.Y1, bounds.Width, bounds.Height);
+                rectangles.Add(rectangle);
+            }
+            while (iterator.Next(pageIteratorLevel));
+
+            iterator.Dispose();
+
+            texts = Translate(texts);
+
+            for (int i = 0; i < texts.Count; i++)
+            {
+                //g.FillRectangle(brushWhite, rectangles[i]);
+                g.DrawRectangle(penBlack, rectangles[i]);
+                g.DrawString(texts[i], drawFont, brushBlack, rectangles[i]);
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bmpArea = bmp.Clone(rectangleArea, PixelFormat.Format16bppRgb555);
+
+            ImageConverter converter = new ImageConverter();
+            byte[] bmpAreaByte = (byte[])converter.ConvertTo(bmpArea, typeof(byte[]));
+
+            string text = "";
+
+            Page page = OCR(bmpAreaByte);
+
+            //Bitmap bmp = new Bitmap(pathImage);
+            Graphics g = Graphics.FromImage(bmp);
+            SolidBrush brushWhite = new SolidBrush(Color.White);
+            SolidBrush brushBlack = new SolidBrush(Color.Black);
+            Pen penBlack = new Pen(Color.Black);
+            Font drawFont = new Font("Times New Roman", 14);
+
+            var iterator = page.GetIterator();
+            PageIteratorLevel pageIteratorLevel = PageIteratorLevel.Block;
+
+            iterator.Begin();
+
+            List<string> texts = new List<string>();
+            List<Rectangle> rectangles = new List<Rectangle>();
+
+            do
+            {
+                text = iterator.GetText(pageIteratorLevel);
+                //text = Translate(text);
+                texts.Add(text);
+
+                iterator.TryGetBoundingBox(pageIteratorLevel, out Rect bounds);
+
+                Rectangle rectangle = new Rectangle(bounds.X1, bounds.Y1, bounds.Width, bounds.Height);
+                rectangles.Add(rectangle);
+            }
+            while (iterator.Next(pageIteratorLevel));
+
+            iterator.Dispose();
+
+            texts = Translate(texts);
+
+            for (int i = 0; i < texts.Count; i++)
+            {
+                Rectangle rectangle = rectangles[i];
+                rectangle.X += rectangleArea.X;
+                rectangle.Y += rectangleArea.Y;
+                //rectangles[i] = rectangle;
+                g.FillRectangle(brushWhite, rectangle);
+                //g.DrawRectangle(penBlack, rectangles[i]);
+                g.DrawString(texts[i], drawFont, brushBlack, rectangle.X, rectangle.Y);
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string text = "";
+
+            Page page = OCR(pathImage);
+            text = page.GetText();
+            textBox2.Text = text;
+            textBox1.Text = Translate(text);
+
+            Bitmap bmp = new Bitmap(pathImage);
+            Graphics g = Graphics.FromImage(bmp);
+            SolidBrush brushWhite = new SolidBrush(Color.White);
+            SolidBrush brushBlack = new SolidBrush(Color.Black);
+            Pen penBlack = new Pen(Color.Black);
+            Font drawFont = new Font("Times New Roman", 16);
+
+            var iterator = page.GetIterator();
+            PageIteratorLevel pageIteratorLevel = PageIteratorLevel.Block;
+
+            iterator.Begin();
+
+            List<string> texts = new List<string>();
+            List<Rectangle> rectangles = new List<Rectangle>();
+
+            do
+            {
+                text = iterator.GetText(pageIteratorLevel);
+                //text = Translate(text);
+                texts.Add(text);
+
+                iterator.TryGetBoundingBox(pageIteratorLevel, out Rect bounds);
+
+                Rectangle rectangle = new Rectangle(bounds.X1, bounds.Y1, bounds.Width, bounds.Height);
+                rectangles.Add(rectangle);
+            }
+            while (iterator.Next(pageIteratorLevel));
+
+            iterator.Dispose();
+
+            texts = Translate(texts);
+
+            for (int i = 0; i < texts.Count; i++)
+            {
+                //g.FillRectangle(brushWhite, rectangles[i]);
+                g.DrawRectangle(penBlack, rectangles[i]);
+                g.DrawString(texts[i], drawFont, brushBlack, rectangles[i]);
+            }
+
+            pictureBox1.Image = bmp;
         }
     }
 }
